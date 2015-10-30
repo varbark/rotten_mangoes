66fc1d741674961bae769fe6c075232ee5fe5c2c
Toto <naoki3355@gmail.com>
2015-10-30 22:36:30 +0000

    finish the search feature

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,6 @@ class Admin::UsersController < ApplicationController
   before_filter :restrict_access
   before_filter :only_admin_access
   def index
-    @users = User.all
-    @user_pages = @users.page(params[:page])
+    @users = User.order(:firstname).page(params[:page]).per(1)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -28,6 +28,55 @@ class Movie < ActiveRecord::Base
     reviews.sum(:rating_out_of_ten) / reviews.size if reviews.size > 0
   end
 
+  class << self
+    def search(search)
+      title = search[:title]
+      director = search[:director]
+      runtime = search[:runtime]
+      return search_title(title).search_director(director).search_runtime_range(runtime)
+    end
+
+    def search_title(title)
+      if title.present?
+        where('title like ?', "%#{title}%")
+    else
+        where('')
+      end
+    end
+
+    def search_director(director)
+      if director.present?
+        where("director like ?", "%#{director}%")
+      else
+        where('')
+      end
+    end
+
+    def search_runtime_range(options)
+      case options
+      when nil
+        where('')
+      when "between90_120"
+        then search_runtime_under(120).search_runtime_over(90)
+      when "over120"
+        then search_runtime_over(120)
+      when "under90"
+        then search_runtime_under(90)
+      else
+        where('')
+      end    
+    end
+    
+    def search_runtime_under(mins)
+      where('runtime_in_minutes <= ?', mins)
+    end
+
+    def search_runtime_over(mins)
+      where('runtime_in_minutes >= ?', mins)
+    end
+    
+  end
+
   protected
 
   def release_date_is_in_the_past
@@ -35,4 +84,5 @@ class Movie < ActiveRecord::Base
       errors.add(:release_date, "should be in the past") if release_date > Date.today
     end
   end
+
 end
