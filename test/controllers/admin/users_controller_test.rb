082b708c9871ee0d8eb32a6f3282db799fda855b
Toto <naoki3355@gmail.com>
2015-10-31 00:36:02 +0000

    some changes(ignore)

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
